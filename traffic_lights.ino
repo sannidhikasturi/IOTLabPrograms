6bd5ff1991c2a95eb02789da9af40beb802a433f
sannidhikasturi <67547611+sannidhikasturi@users.noreply.github.com>
2022-01-09 11:49:49 +0530

    Traffic Lights Simulation

--- traffic_lights.ino
+++ traffic_lights.ino
@@ -2,14 +2,14 @@ int pinRed=13;
 int pinYellow=10;
 int pinGreen=6;
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once :
   pinMode(pinRed,OUTPUT);
   pinMode(pinYellow,OUTPUT);
   pinMode(pinGreen,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly :
   digitalWrite(pinRed,HIGH);
   digitalWrite(pinYellow,LOW);
   digitalWrite(pinGreen,LOW);
@@ -21,5 +21,5 @@ void loop() {
   digitalWrite(pinRed,LOW);
   digitalWrite(pinYellow,LOW);
   digitalWrite(pinGreen,HIGH);
-  delay(1000);
+  delay(1000); 
 }
